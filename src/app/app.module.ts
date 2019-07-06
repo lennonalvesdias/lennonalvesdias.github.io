fa75833888be9ff90379a5ca01bf900c3062ea39
lennonalvesdias <lennonalvesdias@gmail.com>
2019-07-06 17:45:45 -0300

    refactor base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NavbarComponent } from './partials/navbar/navbar.component';
 import { PortfolioComponent } from './pages/profile/portfolio/portfolio.component';
 import { FooterComponent } from './partials/footer/footer.component';
-
 
 @NgModule({
   declarations: [
